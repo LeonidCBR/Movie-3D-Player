63cb88d530e4f0a2fd1a084dff7d961d5cf7f2f9
Leonid Latyshev <forlemonster@gmail.com>
2021-09-11 14:35:17 +1000

    Set up cameras.

--- 3Dplayer/MainViewController.swift
+++ 3Dplayer/MainViewController.swift
@@ -47,7 +47,8 @@ class MainViewController: UIViewController {
 //        camera.zFar = 100.0
         cameraNodeLeft.camera = cameraLeft
         cameraNodeLeft.position = SCNVector3(x: -0.5, y: 0.0, z: 0.0)
-//        cameraNode.eulerAngles.y += .pi/4
+        // turn to the left by 90 degrees
+        cameraNodeLeft.eulerAngles.y += .pi/2
 //        cameraNode.eulerAngles = SCNVector3(x: .pi/20, y: -.pi/4, z: 0)
         sceneLeft.rootNode.addChildNode(cameraNodeLeft)
         sceneViewLeft.pointOfView = cameraNodeLeft
@@ -55,6 +56,8 @@ class MainViewController: UIViewController {
         let cameraRight = SCNCamera()
         cameraNodeRight.camera = cameraRight
         cameraNodeRight.position = SCNVector3(x: 0.5, y: 0.0, z: 0.0)
+        // turn to the right by 90 degrees
+        cameraNodeRight.eulerAngles.y -= .pi/2
         sceneRight.rootNode.addChildNode(cameraNodeRight)
         sceneViewRight.pointOfView = cameraNodeRight
 
@@ -64,21 +67,30 @@ class MainViewController: UIViewController {
 //        let skScene = SKScene(size: CGSize(width: width, height: height))
 
         let geometryLeft = SCNSphere(radius: 30.0)
-        let contentLeft = UIColor.blue //UIImage(named: "picture.jpg")
+//        let contentLeft = UIColor.blue
+        let contentLeft = UIImage(named: "picture.jpg")
         geometryLeft.firstMaterial?.diffuse.contents = contentLeft
+
+        // Flip upside down
+        let matrix = SCNMatrix4MakeRotation(.pi, 1.0, 0.0, 0.0)
+        let transform = SCNMatrix4Translate(matrix, 0.0, 1.0, 1.0)
+        geometryLeft.firstMaterial?.diffuse.contentsTransform = transform
         geometryLeft.firstMaterial?.isDoubleSided = true
         let simpleNodeLeft = SCNNode(geometry: geometryLeft)
         simpleNodeLeft.position = SCNVector3(x: 0.0, y: 0.0, z: 0.0)
-        simpleNodeLeft.pivot = SCNMatrix4MakeRotation(.pi, 1.0, 0.0, 0.0)
+//        let rotation = SCNMatrix4MakeRotation(.pi, 1.0, 0.0, 0.0)
+//        simpleNodeLeft.pivot = rotation
         sceneLeft.rootNode.addChildNode(simpleNodeLeft)
 
         let geometryRight = SCNSphere(radius: 30.0)
-        let contentRight = UIColor.red
+//        let contentRight = UIColor.red
+        let contentRight = contentLeft
         geometryRight.firstMaterial?.diffuse.contents = contentRight
+        geometryRight.firstMaterial?.diffuse.contentsTransform = transform
         geometryRight.firstMaterial?.isDoubleSided = true
         let simpleNodeRight = SCNNode(geometry: geometryRight)
         simpleNodeRight.position = SCNVector3(x: 0.0, y: 0.0, z: 0.0)
-        simpleNodeRight.pivot = SCNMatrix4MakeRotation(.pi, 1.0, 0.0, 0.0)
+//        simpleNodeRight.pivot = rotation
         sceneRight.rootNode.addChildNode(simpleNodeRight)
 
         sceneViewLeft.isPlaying = true
@@ -88,10 +100,13 @@ class MainViewController: UIViewController {
         motionManager.deviceMotionUpdateInterval = 1.0 / 60.0
         motionManager.startDeviceMotionUpdates(to: OperationQueue.main) { deviceMotion, error in
             guard let currentAttitude = deviceMotion?.attitude else { return }
+            // look up at 90 degrees
             let roll = Float(.pi * 0.5 + currentAttitude.roll)
             let yaw = Float(currentAttitude.yaw)
+            let yawRight = yaw + .pi
             let pitch = Float(currentAttitude.pitch)
-            self.cameraNodeLeft.eulerAngles = SCNVector3(x: roll, y: -yaw, z: -pitch)
+            self.cameraNodeLeft.eulerAngles = SCNVector3(x: roll, y: -yaw, z: pitch)
+            self.cameraNodeRight.eulerAngles = SCNVector3(x: roll, y: -yawRight, z: -pitch)
         }
 
     }
