772af6414362adeb51133441ce681d847ca294f7
Leonid Latyshev <forlemonster@gmail.com>
2023-03-31 16:17:42 +1000

    Refactor func application didFinishLaunching.

--- Movie-3D-Player/AppDelegate.swift
+++ Movie-3D-Player/AppDelegate.swift
@@ -11,20 +11,9 @@ import AVFoundation
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-
-
         let audioSession = AVAudioSession.sharedInstance()
-        do {
-            try audioSession.setCategory(.playback, mode: .moviePlayback)
-//            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
-        } catch {
-            print("Setting category to AVAudioSessionCategoryPlayback failed.")
-        }
-
+        try? audioSession.setCategory(.playback, mode: .moviePlayback)
 
 /*
         let videoDirName = "Video"
