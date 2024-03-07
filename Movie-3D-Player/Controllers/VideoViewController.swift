350a3e9cad1c7fe304bfc8bd1caa90cc2f032d03
Leonid Latyshev <forlemonster@gmail.com>
2024-03-07 20:04:02 +1000

    Create video view model.

--- Movie-3D-Player/Controllers/VideoViewController.swift
+++ Movie-3D-Player/Controllers/VideoViewController.swift
@@ -7,26 +7,17 @@
 
 import UIKit
 import SceneKit
-import SpriteKit
-import CoreMotion
-import AVFoundation
-import MediaPlayer
 
-// swiftlint:disable type_body_length
 class VideoViewController: UIViewController {
 
     // MARK: - Properties
-    let rendererDelegate: RendererDelegate
-    let settingsProvider: SettingsProvider
-    let videoPlayer: AVPlayer
-    let motionManager: CMMotionManager
-    let videoFile: Document
+
+    let viewModel: VideoViewModel
 
     let sceneViewLeft: SCNView = {
         let sceneView = SCNView()
         sceneView.backgroundColor = .black
         sceneView.allowsCameraControl = false
-        sceneView.scene = SCNScene(named: SceneProperties.left)
         return sceneView
     }()
 
@@ -34,73 +25,13 @@ class VideoViewController: UIViewController {
         let sceneView = SCNView()
         sceneView.backgroundColor = .black
         sceneView.allowsCameraControl = false
-        sceneView.scene = SCNScene(named: SceneProperties.right)
         return sceneView
     }()
 
-    lazy var cameraNodeLeft: SCNNode = {
-        guard let cameraNode = sceneViewLeft.scene?.rootNode.childNode(withName: SceneProperties.camera,
-                                                                       recursively: false) else {
-            return SCNNode()
-        }
-        return cameraNode
-    }()
-
-    lazy var cameraNodeRight: SCNNode = {
-        guard let cameraNode = sceneViewRight.scene?.rootNode.childNode(withName: SceneProperties.camera,
-                                                                        recursively: false) else {
-            return SCNNode()
-        }
-        return cameraNode
-    }()
-
-    lazy var domeNodeLeft: SCNNode = {
-        guard let domeNode = sceneViewLeft.scene?.rootNode.childNode(withName: SceneProperties.sphere,
-                                                                     recursively: false) else {
-            return SCNNode()
-        }
-        domeNode.eulerAngles = SCNVector3Make(-.pi/2, 0, 0)
-        return domeNode
-    }()
-
-    lazy var domeNodeRight: SCNNode = {
-        guard let domeNode = sceneViewRight.scene?.rootNode.childNode(withName: SceneProperties.sphere,
-                                                                      recursively: false) else {
-            return SCNNode()
-        }
-        domeNode.eulerAngles = SCNVector3Make(-.pi/2, 0, .pi)
-        return domeNode
-    }()
-
-    let videoSKScene: SKScene = {
-        let scene = SKScene(size: CGSize(width: SceneProperties.defaultWidth,
-                                         height: SceneProperties.defaultHeight))
-        scene.scaleMode = .aspectFit
-        return scene
-    }()
-
-    var isPlaying = false {
-        didSet {
-            if isPlaying {
-                videoPlayer.play()
-            } else {
-                videoPlayer.pause()
-            }
-        }
-    }
-
     // MARK: - Lifecycle
 
-    init(with video: Document,
-         settingsProvider: SettingsProvider,
-         motionManager: CMMotionManager,
-         rendererDelegate: RendererDelegate) {
-        self.videoFile = video
-        let videoItem = AVPlayerItem(url: video.fileURL)
-        self.videoPlayer = AVPlayer(playerItem: videoItem)
-        self.settingsProvider = settingsProvider
-        self.motionManager = motionManager
-        self.rendererDelegate = rendererDelegate
+    init(with videoViewModel: VideoViewModel) {
+        self.viewModel = videoViewModel
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -108,8 +39,12 @@ class VideoViewController: UIViewController {
         return nil
     }
 
+    deinit {
+        UIApplication.shared.isIdleTimerDisabled = false
+    }
+
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        switch settingsProvider.orientation {
+        switch viewModel.orientation {
         case .leftSideDown:
             return .landscapeRight
         case .rightSideDown:
@@ -125,24 +60,22 @@ class VideoViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .black
         UIApplication.shared.isIdleTimerDisabled = true
-        videoPlayer.preventsDisplaySleepDuringVideoPlayback = true
-        initScene()
+        configureUI()
+        viewModel.configurePlayer()
         configureGestures()
-        setupRemoteTransportControls()
-        setupNowPlaying()
-        isPlaying = true
+        viewModel.play()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         sceneViewLeft.isPlaying = true
         sceneViewRight.isPlaying = true
-        motionManager.startDeviceMotionUpdates(using: .xMagneticNorthZVertical)
+        viewModel.startDeviceMotionUpdates()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        motionManager.stopDeviceMotionUpdates()
+        viewModel.stopDeviceMotionUpdates()
         sceneViewLeft.isPlaying = false
         sceneViewRight.isPlaying = false
     }
@@ -152,7 +85,7 @@ class VideoViewController: UIViewController {
         // calculate with of the scene's views
         let height = view.frame.height
         let halfWidth = view.frame.width / 2
-        let space = settingsProvider.space
+        let space = viewModel.space
         let widthOfSceneView = halfWidth - space / 2 - view.safeAreaLayoutGuide.layoutFrame.minX
         let length = height < widthOfSceneView ? height : widthOfSceneView
         // calculate position of the left scene's view
@@ -165,111 +98,62 @@ class VideoViewController: UIViewController {
         sceneViewRight.frame = CGRect(x: xPosRightEye, y: yPosRigthEye, width: length, height: length)
     }
 
-    deinit {
-        UIApplication.shared.isIdleTimerDisabled = false
-    }
-
     // MARK: - Methods
 
-    func initScene() {
-        let videoSKNode = SKVideoNode(avPlayer: videoPlayer)
-        videoSKNode.position = CGPoint(x: SceneProperties.defaultWidth / 2,
-                                       y: SceneProperties.defaultHeight / 2)
-        videoSKNode.size = videoSKScene.size
-        videoSKScene.addChild(videoSKNode)
-        domeNodeLeft.geometry?.firstMaterial?.diffuse.contents = videoSKScene
-        domeNodeRight.geometry?.firstMaterial?.diffuse.contents = videoSKScene
-        initScenePosition()
-        configureSceneViews()
-        configureCameras()
-    }
-
-    func initScenePosition() {
-        domeNodeLeft.eulerAngles.z = cameraNodeLeft.eulerAngles.z
-        domeNodeRight.eulerAngles.z = cameraNodeRight.eulerAngles.z + .pi
-    }
-
-    func configureSceneViews() {
+    func configureUI() {
         sceneViewLeft.translatesAutoresizingMaskIntoConstraints = false
         sceneViewRight.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(sceneViewLeft)
         view.addSubview(sceneViewRight)
-        sceneViewLeft.delegate = rendererDelegate
-        sceneViewRight.delegate = rendererDelegate
-    }
-
-    func configureCameras() {
-        cameraNodeLeft.camera?.fieldOfView = settingsProvider.fieldOfView
-        cameraNodeRight.camera?.fieldOfView = settingsProvider.fieldOfView
-    }
-
-    func setupRemoteTransportControls() {
-        let commandCenter = MPRemoteCommandCenter.shared()
-        // Add handler for Play Command
-        commandCenter.playCommand.addTarget { [weak self] _ in // event
-            guard let self = self else {
-                return .noActionableNowPlayingItem
-            }
-            if self.videoPlayer.rate == 0.0 {
-                self.videoPlayer.play()
-                return .success
-            }
-            return .commandFailed
-        }
-        // Add handler for Pause Command
-        commandCenter.pauseCommand.addTarget { [weak self] _ in // event
-            guard let self = self else {
-                return .noActionableNowPlayingItem
-            }
-            if self.videoPlayer.rate == 1.0 {
-                self.videoPlayer.pause()
-                return .success
-            }
-            return .commandFailed
-        }
-    }
-
-    func setupNowPlaying() {
-        var nowPlayingInfo = [String: Any]()
-        nowPlayingInfo[MPMediaItemPropertyTitle] = videoFile.localizedName
-//        if let image = UIImage(named: "picture.jpg") {
-//            nowPlayingInfo[MPMediaItemPropertyArtwork] =
-//            MPMediaItemArtwork(boundsSize: image.size) { size in
-//                return image
-//            }
-//        }
-        nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = videoPlayer.currentItem?.currentTime().seconds
-        nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = videoPlayer.currentItem?.duration.seconds
-        nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = videoPlayer.rate
-        MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+        sceneViewLeft.scene = viewModel.leftScene
+        sceneViewRight.scene = viewModel.rightScene
+        sceneViewLeft.delegate = viewModel.rendererDelegate
+        sceneViewRight.delegate = viewModel.rendererDelegate
     }
 
     override func pressesEnded(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
         guard let key = presses.first?.key else { return }
         switch key.keyCode {
         case .keyboardLeftArrow:
-            seekBackward(by: 20)
+            viewModel.seekBackward(by: 20)
         case .keyboardRightArrow:
-            seekForward(by: 20)
+            viewModel.seekForward(by: 20)
         default:
             super.pressesEnded(presses, with: event)
         }
     }
 
-    func seekBackward(by seconds: CMTimeValue) {
-        let delta = CMTime(value: seconds, timescale: 1)
-        let newTime = videoPlayer.currentTime() - delta
-        videoPlayer.seek(to: newTime)
-    }
-
-    func seekForward(by seconds: CMTimeValue) {
-        let delta = CMTime(value: seconds, timescale: 1)
-        let newTime = videoPlayer.currentTime() + delta
-        videoPlayer.seek(to: newTime)
-    }
-
     func configureGestures() {
+/*
+
         // TODO: Consider to refactor it
+        // and move to VM
+        // There would be only gestures
+
+        for gesture in PlayerGesture.allCases {
+            switch gesture {
+            case .none:
+                break
+            case .singleTap:
+                <#code#>
+            case .singleTapTwoFingers:
+                <#code#>
+            case .swipeUp:
+                <#code#>
+            case .swipeDown:
+                <#code#>
+            case .swipeLeft:
+                <#code#>
+            case .swipeRight:
+                <#code#>
+            case .swipeUpTwoFingers:
+                <#code#>
+            case .swipeDownTwoFingers:
+                <#code#>
+            }
+        }
+
+
 //        let myActions: [PlayerAction: Selector]
 //        for action in PlayerAction.allCases {
 //            switch action {
@@ -315,6 +199,7 @@ class VideoViewController: UIViewController {
                 view.addGestureRecognizer(gesture)
             }
         }
+*/
     }
 
     func getSingleTapGesture(_ selector: Selector) -> UIGestureRecognizer {
@@ -375,11 +260,16 @@ class VideoViewController: UIViewController {
 
     // MARK: - Selectors
 
+    // TODO: Or we should use like:
+    // func handleSwipeDown {
+    // ...
+    // viewModel.handle(.swipeDown)
+
     /** Play/pause */
     @objc func handlePlay(_ gestureRecognizer: UIGestureRecognizer) {
         guard gestureRecognizer.view != nil else { return }
         if gestureRecognizer.state == .ended {
-            isPlaying.toggle()
+            viewModel.isPlaying.toggle()
         }
     }
 
@@ -387,7 +277,7 @@ class VideoViewController: UIViewController {
     @objc func handleInitScenePosition(_ gestureRecognizer: UIGestureRecognizer) {
         guard gestureRecognizer.view != nil else { return }
         if gestureRecognizer.state == .ended {
-            initScenePosition()
+            viewModel.initScenePosition()
         }
     }
 
@@ -395,14 +285,7 @@ class VideoViewController: UIViewController {
     @objc func handleIncreaseFOV(_ gestureRecognizer: UIGestureRecognizer) {
         guard gestureRecognizer.view != nil else { return }
         if gestureRecognizer.state == .ended {
-            if let leftCamera = cameraNodeLeft.camera,
-               leftCamera.fieldOfView < SettingsProperties.FieldOfView.maxThreshold {
-                leftCamera.fieldOfView += 5
-            }
-            if let rightCamera = cameraNodeRight.camera,
-               rightCamera.fieldOfView < SettingsProperties.FieldOfView.maxThreshold {
-                rightCamera.fieldOfView += 5
-            }
+            viewModel.increaseFOV()
         }
     }
 
@@ -410,14 +293,7 @@ class VideoViewController: UIViewController {
     @objc func handleDecreaseFOV(_ gestureRecognizer: UIGestureRecognizer) {
         guard gestureRecognizer.view != nil else { return }
         if gestureRecognizer.state == .ended {
-            if let leftCamera = cameraNodeLeft.camera,
-               leftCamera.fieldOfView > SettingsProperties.FieldOfView.minThreshold {
-                leftCamera.fieldOfView -= 5
-            }
-            if let rightCamera = cameraNodeRight.camera,
-               rightCamera.fieldOfView > SettingsProperties.FieldOfView.minThreshold {
-                rightCamera.fieldOfView -= 5
-            }
+            viewModel.decreaseFOV()
         }
     }
 
@@ -425,7 +301,7 @@ class VideoViewController: UIViewController {
     @objc func handleRewindBackward(_ gestureRecognizer: UIGestureRecognizer) {
         guard gestureRecognizer.view != nil else { return }
         if gestureRecognizer.state == .ended {
-            seekBackward(by: 20)
+            viewModel.seekBackward(by: 20)
         }
     }
 
@@ -433,7 +309,7 @@ class VideoViewController: UIViewController {
     @objc func handleRewindForward(_ gestureRecognizer: UIGestureRecognizer) {
         guard gestureRecognizer.view != nil else { return }
         if gestureRecognizer.state == .ended {
-            seekForward(by: 20)
+            viewModel.seekForward(by: 20)
         }
     }
 
@@ -446,4 +322,3 @@ class VideoViewController: UIViewController {
     }
 
 }
-// swiftlint:enable type_body_length
